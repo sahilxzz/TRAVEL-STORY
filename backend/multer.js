8550f252b78f18e6c8cfe650c7f49c528b744175
Sahil <codekage9@gmail.com>
2025-07-05 09:51:57 +0530

    new changes

--- backend/multer.js
+++ backend/multer.js
@@ -1,29 +1,38 @@
-const multer = require('multer');
-const path = require('path');
+// const multer = require('multer');
+// const path = require('path');
 
 // Storage configuration
 
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, "./uploads/");  // Destination folder for storing uploaded files
-    },
-    filename: function (req, file, cb){
-        cb(null, Date.now() + path.extname(file.originalname));  // unique filename
-    },
-});
+// const storage = multer.diskStorage({
+//     destination: function (req, file, cb) {
+//         cb(null, "./uploads/");  // Destination folder for storing uploaded files
+//     },
+//     filename: function (req, file, cb){
+//         cb(null, Date.now() + path.extname(file.originalname));  // unique filename
+//     },
+// });
 
 // file filter to accept only images
 
-const fileFilter = (req, file, cb) => {
-    if(file.mimetype.startsWith("image/")){
-        cb(null, true);
-    }else{
-        cb(new Error("Only images are allowed"), false);
-    }
-};
+// const fileFilter = (req, file, cb) => {
+//     if(file.mimetype.startsWith("image/")){
+//         cb(null, true);
+//     }else{
+//         cb(new Error("Only images are allowed"), false);
+//     }
+// };
 
 // initialize multer instance
 
-const upload = multer({storage, fileFilter});
+// const upload = multer({storage, fileFilter});
+
+
+// module.exports = upload;
+
+
+const multer = require("multer");
+const { storage } = require("./cloudinary");
+
+const upload = multer({ storage });
 
 module.exports = upload;
--- frontend/travel-story-app/src/components/input/SearchBar.jsx
+++ frontend/travel-story-app/src/components/input/SearchBar.jsx
@@ -6,6 +6,7 @@ const SearchBar = ({value, onChange, handleSearch, onClearSearch}) => {
   return (
     <div className='w-80 flex items-center px-4 bg-slate-100 rounded-md'>
         <input type="text" 
+        id="search"
         placeholder='Search Notes'
         className='w-full text-xs bg-transparent py-[11px] outline-none'
         value={value}
